509d43bc12600e21a805776704d705f79856dff4
Dam <shit2wice@gmail.com>
2022-08-09 23:00:00 +0100

    feat: Add search component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import Search from "./component/search/Search";
 import Address from "./component/address/Address";
 import Map from "./component/map/Map";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <div className="app">
       <div className="app__header">
         <div className="app__header-text">IP Address Tracker</div>
         <div className="app__header-search">
-          <Search />
+          <Search search={search} setSearch={setSearch} />
         </div>
         <div className="app__header-address">
           <Address />
--- src/component/search/Search.js
+++ src/component/search/Search.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Search.css";
+import icon from "../../images/icon-arrow.svg";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ search, setSearch }) => {
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className="search">
+      <input
+        name="search"
+        value={search}
+        onChange={handleChange}
+        className="search__input"
+      />
+      <div className="search__icon">
+        <img src={icon} alt="right arrow" />
+      </div>
+    </div>
+  );
 };
 
 export default Search;

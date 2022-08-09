382478bb836a401c2701bce280e4f59d806ad09f
Dam <shit2wice@gmail.com>
2022-08-09 19:32:45 +0100

    feat: Add mobile layout containers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
+import Search from "./component/search/Search";
+import Address from "./component/address/Address";
+import Map from "./component/map/Map";
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      {/* Search */}
-      {/* Address */}
-      {/* MapView */}
+      <div className="app__header">
+        <div className="app__header-text">IP Address Tracker</div>
+        <div className="app__header-search">
+          <Search />
+        </div>
+        <div className="app__header-address">
+          <Address />
+        </div>
+      </div>
+      <div className="app__map">
+        <Map />
+      </div>
     </div>
   );
 }
